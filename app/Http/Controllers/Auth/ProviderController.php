c35e0e97a3142e98cdb4bb4c3da04e4c1a02a56c
Jeph2001 <josephmaniragaba9@gmail.com>
2024-03-28 12:35:06 +0200

    my portfolio-not completed yet

---
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class ProviderController extends Controller
+{
+    public function redirect($provider){
+
+        return Socialite::driver($provider)->redirect();
+
+    }
+
+    public function dashboard(){
+        return view('dashboard');
+    }
+
+    public function callback($provider){
+        $socialiteUser = Socialite::driver($provider)->user();
+    
+        return redirect('/dashboard');
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Auth\ProviderController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+ 
+Route::get('/auth/{provider}/redirect', [ProviderController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [ProviderController::class, 'callback']);
+Route::get('/dashboard', [ProviderController::class, 'dashboard']);
+
